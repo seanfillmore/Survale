ca03fd8b349ebedf93699fb861c6f5af9397d62c
Sean Fillmore <sean.fillmore@ventura.org>
2024-11-16 15:43:30 -0800

    Initial Commit

---
+++ Survale/SurvaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurvaleApp.swift
+//  Survale
+//
+//  Created by Sean Fillmore on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SurvaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SurvaleTests/SurvaleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SurvaleTests.swift
+//  SurvaleTests
+//
+//  Created by Sean Fillmore on 11/16/24.
+//
+
+import Testing
+@testable import Survale
+
+struct SurvaleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
